675baf25edbdb8d781ba70c339db4a4909eeec7d
MAC <mac@MACde-MacBook-Pro-5.local>
2020-01-04 18:00:59 +0800

    first version of jalan

---
+++ jalan.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function(){//使用JQ語法，JQ ready的function是必要的
+    $(function(){ $('#btn-discover').click(function(){ 
+        $('html,body').animate({scrollTop:$(".card").offset().top}, 500); 
+        });  
+    }); 
+});
+
+
+
+$(document).ready(function(){
+    $(function(){ $(".click-learn").click(function(){ 
+        $('html,body').animate({scrollTop:$(".scroll-learn").offset().top}, 500); 
+        });  
+    }); 
+});
+
+
+
+
