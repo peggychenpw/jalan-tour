f9c0e416094f9c8eb7220c7ab7143a5555f31638
MAC <mac@MACde-MacBook-Pro-5.local>
2020-01-06 09:44:17 +0800

    change image

--- jalan.js
+++ jalan.js
@@ -8,12 +8,7 @@ $(document).ready(function(){//使用JQ語法，JQ ready的function是必要的
 
 
 
-$(document).ready(function(){
-    $(function(){ $(".click-learn").click(function(){ 
-        $('html,body').animate({scrollTop:$(".scroll-learn").offset().top}, 500); 
-        });  
-    }); 
-});
+$(document).ready(function(){$(function(){$(".click-learn").click(function(){$("html,body").animate({scrollTop:$(".scroll-learn").offset().top},500)})})});
 
 
 
